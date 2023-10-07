cb723b6fd1987eaf3a25e8d77752825249db3f91
okue_hidetaka <okue@as-systems.jp>
2023-10-07 21:57:49 +0900

    各画面のファイル作成

---
+++ DiaryEnglish_iOS/viewControllers/DiaryEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiaryEditViewController.swift
+//  DiaryEnglish_iOS
+//
+//  Created by 奥江英隆 on 2023/10/07.
+//
+
+import UIKit
+
+class DiaryEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
