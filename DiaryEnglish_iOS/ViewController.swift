3c302c2b569bcf9737771abd5520e738971ce426
okue_hidetaka <okue@as-systems.jp>
2023-10-07 21:46:38 +0900

    firstCommit

---
+++ DiaryEnglish_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryEnglish_iOS
+//
+//  Created by 奥江英隆 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
